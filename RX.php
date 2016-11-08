07acedda8804f4e0086c755cd6d4f3888d2d6290
Rahu <rahul4her@hotmail.com>
2016-11-09 02:57:50 +0530

    RX Utils added

--- RX.php
+++ RX.php
@@ -1,19 +1,11 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: lalittanwar
- * Date: 25/10/16
- * Time: 3:54 PM
- */
-
-namespace app;
+namespace RudraX;
 
 class RX
 {
 
     public static function handler($handle_name)
     {
-        $CloudinaryClassInfo = \ClassUtil::getModel($handle_name);
+        $CloudinaryClassInfo = \ClassUtil::getHandler($handle_name);
         if ($CloudinaryClassInfo != NULL) {
             include_once $CloudinaryClassInfo["filePath"];
             $CloudinaryClassInstance = new $CloudinaryClassInfo["className"]();
